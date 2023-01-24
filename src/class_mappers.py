332e277a7bcf91685fe8b2f06a43a6f72be0353d
Maciej <wrobcio789@gmail.com>
2023-01-24 02:26:44 +0100

    Add support for multilabel segmentation and mask merging

--- src/class_mappers.py
+++ src/class_mappers.py
@@ -1,19 +1,45 @@
 from abc import ABC, abstractmethod
+from typing import List, Dict
 
 class AbstractClassMapper(ABC):
     @abstractmethod
-    def map(self, name: str) -> str:
+    def map(self, name: str) -> List[str]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_healthy(self, name: str) -> bool:
         raise NotImplementedError
 
 class DictClassMapper(AbstractClassMapper):
 
-    def __init__(self, mappings) -> None:
+    def __init__(self, mappings: Dict) -> None:
         super().__init__()
-        self.mappings = mappings
+        self.class_mappings = {}
+        self.healthy_mappings = {}
 
-    def map(self, name: str) -> str:
-        return self.mappings[name] if name in self.mappings else None
+        for output_class, configuration in mappings.items():
+            self.healthy_mappings[output_class] = configuration.get('healthy', False)
+            for input_class in configuration['classes']:
+                if input_class in self.class_mappings:
+                    self.class_mappings[input_class].append(output_class)
+                else:
+                    self.class_mappings[input_class] = [output_class]
+            
+
+    def map(self, name: str) -> List[str]:
+        return self.class_mappings[name] if name in self.class_mappings else []
+
+    def is_healthy(self, name: str) -> bool:
+        return self.healthy_mappings.get(name, False)
+
 
 class DummyClassMapper(AbstractClassMapper):
-    def map(self, name: str) -> str:
-        return name
+
+    def __init__(self):
+        self.healthy_classes = ['h01', 'h02', 'h03', 'h04', 'h05', 'h06', 'h07', 'b02']
+
+    def map(self, name: str) -> List[str]:
+        return [name]
+    
+    def is_healthy(self, name: str) -> bool:
+        return name in self.healthy_classes
--- src/ers_preparator.py
+++ src/ers_preparator.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 import yaml
-from typing import List, Tuple
+from typing import List, Dict
 from src.class_mappers import AbstractClassMapper, DummyClassMapper, DictClassMapper
 
 class ErsPreparator:
@@ -12,9 +12,7 @@ class ErsPreparator:
         self.use_seq = args.ers_use_seq
         self.use_empty_masks = args.ers_use_empty_masks
         self.binary = args.binary
-        self.healthy_classes = ['h01', 'h02', 'h03', 'h04', 'h05', 'h06', 'h07', 'b02']
         
-
     def generate_dataframe(self) -> pd.DataFrame:
         if not self.dataset_path:
             return pd.DataFrame()
@@ -28,32 +26,66 @@ class ErsPreparator:
                 if not os.path.isdir(labels_dir):
                     continue
 
-                for mask_path in self.__list_files(labels_dir):
-                    accept_healthy_only = (not self.use_empty_masks) and os.path.getsize(mask_path) == 0
-                    mask_basename = os.path.basename(mask_path)
-                    img_path = os.path.join(frames_dir, mask_basename[:6] + ".png")
-                    for class_name, isHealthy in self.__extract_classes_from_mask_name(mask_basename, accept_healthy_only):
-                        data.append(
-                            {
-                                'patient_id': patient_id,
-                                'class': class_name,
-                                'img_path': img_path,
-                                'mask_path': mask_path,
-                                'reverse_mask': not (self.binary or (not isHealthy)),
-                                'dataset': 'ers'
-                            }
-                        )
+                frame_to_masks = self.__create_frame_to_masks_dict(frames_dir, labels_dir)
+                frame_to_class_records = self.__create_frame_to_class_records_dict(frame_to_masks)
+                for frame_path, class_records in frame_to_class_records.items():
+                    self.__merge_class_records_and_append_to_data(data, frame_path, class_records, patient_id)
 
         return pd.DataFrame(data)
 
-    def __extract_classes_from_mask_name(self, mask_basename: str, only_healthy: bool) -> List[Tuple[str, bool]]:
+    def __create_frame_to_masks_dict(self, frames_dir: str, labels_dir: str) -> Dict:
+        frame_to_masks = {}
+        frames_paths = self.__list_files(frames_dir)
+        masks_paths = self.__list_files(labels_dir)
+        for frame_path in frames_paths:
+            frame_name = os.path.splitext(os.path.basename(frame_path))[0]
+            masks_for_frame = [mask_path for mask_path in masks_paths if os.path.basename(mask_path).startswith(frame_name)]
+            frame_to_masks[frame_path] = masks_for_frame
+        return frame_to_masks
+
+    def __create_frame_to_class_records_dict(self, frame_to_masks: Dict) -> Dict:
+        frame_to_class_records = {}
+        for frame_path, masks_paths in frame_to_masks.items():
+            class_records_for_frame = []
+            for mask_path in masks_paths:
+                mask_basename = os.path.basename(mask_path)
+                accept_healthy_only = (not self.use_empty_masks) and os.path.getsize(mask_path) == 0
+                for class_name in self.__extract_classes_from_mask_name(mask_basename, accept_healthy_only):
+                    class_records_for_frame.append({
+                        'class': class_name,
+                        'mask_path': mask_path,
+                    })
+            frame_to_class_records[frame_path] = class_records_for_frame
+        return frame_to_class_records
+
+    def __merge_class_records_and_append_to_data(self, data: Dict, frame_path: str, class_records: List[Dict], patient_id: str) -> None:
+        processed_class_names = set()
+        all_classes = {class_record['class'] for class_record in class_records}
+        for class_name in all_classes:
+            if class_name in processed_class_names:
+                continue
+            processed_class_names.add(class_name)
+
+            records = [record for record in class_records if record['class'] == class_name]
+            is_class_healthy = self.class_mapper.is_healthy(class_name)
+            masks_paths_for_class = list(set([record['mask_path'] for record in records]))
+
+            data.append({
+                'patient_id': patient_id,
+                'class': class_name,
+                'img_path': frame_path,
+                'masks_paths': masks_paths_for_class,
+                'reverse_mask': not (self.binary or (not is_class_healthy)),
+                'dataset': 'ers'
+            })
+
+    def __extract_classes_from_mask_name(self, mask_basename: str, only_healthy: bool) -> List[str]:
         mask_name_without_extension = os.path.splitext(mask_basename)[0]
         unprocessed_class_names = [class_name for class_name in mask_name_without_extension.split('_') if len(class_name) == 3]
-        if only_healthy:
-            unprocessed_class_names = [class_name for class_name in unprocessed_class_names if class_name in self.healthy_classes]
-        mapped_class_names = [(self.class_mapper.map(class_name), class_name in self.healthy_classes) for class_name in unprocessed_class_names]
-        return {(class_name, isHealthy) for (class_name, isHealthy) in mapped_class_names if class_name is not None}
 
+        mapped_class_names = self.__flatten_and_unique([self.class_mapper.map(class_name) for class_name in unprocessed_class_names])
+        mapped_class_names = mapped_class_names if not only_healthy else [class_name for class_name in mapped_class_names if self.class_mapper.is_healthy(class_name)]
+        return mapped_class_names
 
     def __get_data_dirs(self, patient_dir: str, use_seq: bool) -> List[str]:
         if use_seq:
@@ -65,6 +97,9 @@ class ErsPreparator:
 
     def __list_files(self, path: str) -> List[str]:
         return [os.path.join(path, dirname) for dirname in os.listdir(path) if os.path.isfile(os.path.join(path, dirname))]
+
+    def __flatten_and_unique(self, colection):
+        return list({item for sublist in colection for item in sublist})
 
     @staticmethod
     def __create_class_mapper(class_mapper_path: str) -> AbstractClassMapper: 
